d5e4b778829116e67decda85e1a24d3e4be99f2b
Bryan Fullam <bryanfullam@gmail.com>
2024-08-04 17:08:58 +0200

    Transition to using HybridButtons

--- app/services/buttons.tsx
+++ app/services/buttons.tsx
@@ -1,9 +1,32 @@
 import { Button } from "frames.js/next";
 import { DeploymentOption, YamlData } from "../types/yamlData";
 
+interface HybridButtonProps
+  extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  isWebapp: boolean;
+  target?: any;
+  children?: any;
+}
+
+export const HybridButton = ({
+  isWebapp,
+  target,
+  children,
+  ...rest
+}: HybridButtonProps) => {
+  return isWebapp ? (
+    <button {...rest}>{children}</button>
+  ) : (
+    <Button action={"post"} target={{ query: target }}>
+      {children}
+    </Button>
+  );
+};
+
 export const getPaginatedButtons = (
   allButtons: any[],
-  buttonPage: number
+  buttonPage: number,
+  isWebapp: boolean | undefined = false
 ): any[] => {
   const buttonPageOffset = buttonPage * 3;
   let buttonEndIndex = buttonPageOffset + 4;
@@ -15,52 +38,50 @@ export const getPaginatedButtons = (
     ...(allButtons.length <= 3
       ? allButtons
       : [
-          buttonPage > 0 && (
-            <Button
-              action="post"
-              target={{ query: { buttonPage: buttonPage - 1 } }}
-            >
-              {"<"}
-            </Button>
-          ),
+          buttonPage > 0 &&
+            HybridButton({
+              isWebapp,
+              target: { buttonPage: buttonPage - 1 },
+              children: "<",
+            }),
           ...allButtons.slice(buttonPageOffset, buttonEndIndex),
-          includeMoreButton && (
-            <Button
-              action="post"
-              target={{ query: { buttonPage: buttonPage + 1 } }}
-            >
-              {"More"}
-            </Button>
-          ),
+          includeMoreButton &&
+            HybridButton({
+              isWebapp,
+              target: { buttonPage: buttonPage + 1 },
+              children: "More",
+            }),
         ]),
   ];
 };
 
 export const getPresetsButtons = (
   presets: number[],
-  minimum: number | undefined
+  minimum: number | undefined,
+  isWebapp: boolean | undefined = false
 ): any[] => {
   return [
-    <Button action="post" target={{ query: { buttonValue: "back" } }}>
-      {"<"}
-    </Button>,
-    ...presets.map((preset: number) => (
-      <Button action="post" target={{ query: { buttonValue: `${preset}` } }}>
-        {String(preset)}
-      </Button>
-    )),
+    HybridButton({
+      isWebapp: false,
+      target: { buttonValue: "back" },
+      children: "<",
+    }),
+    ...presets.map((preset: number) =>
+      HybridButton({
+        isWebapp: false,
+        target: { buttonValue: `${preset}` },
+        children: `${preset}`,
+      })
+    ),
     ...(minimum !== undefined
       ? [
-          <Button
-            action="post"
-            target={{
-              query: {
-                textInputLabel: `Enter a number greater than ${minimum}`,
-              },
-            }}
-          >
-            Custom
-          </Button>,
+          HybridButton({
+            isWebapp,
+            target: {
+              textInputLabel: `Enter a number greater than ${minimum}`,
+            },
+            children: "Custom",
+          }),
         ]
       : []),
   ];
@@ -74,34 +95,36 @@ export const generateButtons = (
   let buttons: any[] = [];
   if (currentState.textInputLabel) {
     return [
-      <Button action="post" target={{ query: { textInputLabel: "" } }}>
-        {"<"}
-      </Button>,
-      <Button action="post" target={{ query: { buttonValue: "submit" } }}>
-        Submit
-      </Button>,
+      HybridButton({
+        isWebapp,
+        target: { textInputLabel: "" },
+        children: "<",
+      }),
+      HybridButton({
+        isWebapp,
+        target: { buttonValue: "submit" },
+        children: "Submit",
+      }),
     ];
   }
   switch (currentState.currentStep) {
     case "start":
       buttons = [
-        <Button action="post" target={{ query: { buttonValue: "start" } }}>
-          Start
-        </Button>,
+        HybridButton({
+          isWebapp,
+          target: { buttonValue: "start" },
+          children: "Start",
+        }),
       ];
       break;
     case "deployment":
       const allButtons = yamlData.gui.deployments.map(
-        (deploymentOption: DeploymentOption) => (
-          <Button
-            action="post"
-            target={{
-              query: { buttonValue: JSON.stringify(deploymentOption) },
-            }}
-          >
-            {deploymentOption.name}
-          </Button>
-        )
+        (deploymentOption: DeploymentOption) =>
+          HybridButton({
+            isWebapp,
+            target: { buttonValue: JSON.stringify(deploymentOption) },
+            children: deploymentOption.name,
+          })
       );
       buttons = getPaginatedButtons(allButtons, currentState.buttonPage);
       break;
@@ -120,19 +143,25 @@ export const generateButtons = (
       break;
     case "review":
       buttons = [
-        <Button action="post" target={{ query: { buttonValue: "back" } }}>
-          {"<"}
-        </Button>,
-        <Button action="post" target={{ query: { buttonValue: "submit" } }}>
-          Submit
-        </Button>,
+        HybridButton({
+          isWebapp,
+          target: { buttonValue: "back" },
+          children: "<",
+        }),
+        HybridButton({
+          isWebapp,
+          target: { buttonValue: "submit" },
+          children: "Submit",
+        }),
       ];
       break;
     case "done":
       buttons = [
-        <Button action="post" target={{ query: { buttonValue: "restart" } }}>
-          Start over
-        </Button>,
+        HybridButton({
+          isWebapp,
+          target: { buttonValue: "restart" },
+          children: "Start over",
+        }),
       ];
       break;
   }

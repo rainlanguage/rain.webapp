28df763ae4fd211f7ac8e7ea31b46eace2f28916
Bryan Fullam <bryanfullam@gmail.com>
2024-08-05 11:28:43 +0200

    Improve styling

--- app/UI/WebappFrame.tsx
+++ app/UI/WebappFrame.tsx
@@ -26,55 +26,55 @@ const WebappFrame = ({ yamlData }: props) => {
   const buttonsData = generateButtonsData(yamlData, currentState);
   return (
     <>
-      <div className="w-1/4 ml-10">
+      <div className="w-1/2 ml-10">
         <FrameImage currentState={currentState} />
-      </div>
-      {currentState.textInputLabel && (
-        <>
-          <input
-            type="number"
-            placeholder={currentState.textInputLabel}
-            value={inputText}
-            onChange={(e) => setInputText(e.target.value)}
-          />
-          <br />
-        </>
-      )}
-      {buttonsData.map((buttonData) => (
-        <button
-          key={buttonData.buttonText}
-          className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded"
-          onClick={() => {
-            // Handle page navigation
-            if (buttonData.buttonTarget === "textInputLabel") {
-              setCurrentState({
-                ...currentState,
-                textInputLabel: buttonData.buttonValue,
-              });
-              return;
-            } else if (buttonData.buttonTarget === "buttonPage") {
-              setCurrentState({
-                ...currentState,
-                buttonPage: buttonData.buttonValue,
-              });
-              return;
-            }
+        {currentState.textInputLabel && (
+          <>
+            <input
+              type="number"
+              placeholder={currentState.textInputLabel}
+              value={inputText}
+              onChange={(e) => setInputText(e.target.value)}
+            />
+            <br />
+          </>
+        )}
+        {buttonsData.map((buttonData) => (
+          <button
+            key={buttonData.buttonText}
+            className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded"
+            onClick={() => {
+              // Handle page navigation
+              if (buttonData.buttonTarget === "textInputLabel") {
+                setCurrentState({
+                  ...currentState,
+                  textInputLabel: buttonData.buttonValue,
+                });
+                return;
+              } else if (buttonData.buttonTarget === "buttonPage") {
+                setCurrentState({
+                  ...currentState,
+                  buttonPage: buttonData.buttonValue,
+                });
+                return;
+              }
 
-            const updatedState = getUpdatedFrameState(
-              yamlData,
-              currentState,
-              buttonData.buttonValue,
-              inputText
-            );
-            setCurrentState({ ...updatedState });
-            if (inputText) {
-              setInputText("");
-            }
-          }}
-        >
-          {buttonData.buttonText}
-        </button>
-      ))}
+              const updatedState = getUpdatedFrameState(
+                yamlData,
+                currentState,
+                buttonData.buttonValue,
+                inputText
+              );
+              setCurrentState({ ...updatedState });
+              if (inputText) {
+                setInputText("");
+              }
+            }}
+          >
+            {buttonData.buttonText}
+          </button>
+        ))}
+      </div>
     </>
   );
 };

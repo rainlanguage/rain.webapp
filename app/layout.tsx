b0734d0a5c195c3aea8560bde5ee4054318471b7
Bryan Fullam <bryanfullam@gmail.com>
2024-08-08 14:45:12 +0200

    Add rainbowkit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { Providers } from "./providers";
+import { ConnectButton } from "@rainbow-me/rainbowkit";
+import "@rainbow-me/rainbowkit/styles.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +19,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Providers>
+          <div style={{ display: "flex", justifyContent: "flex-end" }}>
+            <ConnectButton />
+          </div>
+          {children}
+        </Providers>
+      </body>
     </html>
   );
 }
---
+++ app/providers.tsx
@@ -0,0 +1,32 @@
+"use client";
+import * as React from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { WagmiProvider } from "wagmi";
+import { getDefaultConfig, RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import {
+  mainnet,
+  polygon,
+  optimism,
+  arbitrum,
+  base,
+  flare,
+} from "wagmi/chains";
+
+const config = getDefaultConfig({
+  appName: "RainFrame",
+  projectId: "ba29a4b0642a94fd4dbc754841c2decb",
+  chains: [mainnet, polygon, optimism, arbitrum, base, flare],
+  ssr: true, // If your dApp uses server side rendering (SSR)
+});
+
+const queryClient = new QueryClient();
+
+export function Providers({ children }: { children: React.ReactNode }) {
+  return (
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+        <RainbowKitProvider>{children}</RainbowKitProvider>
+      </QueryClientProvider>
+    </WagmiProvider>
+  );
+}

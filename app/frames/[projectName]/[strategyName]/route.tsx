cdc1466534d9721d717104fd242666d3dbd5ad3d
Bryan Fullam <bryanfullam@gmail.com>
2024-08-08 15:04:18 +0200

    Extract createFrames to separate file

--- app/frames/[projectName]/[strategyName]/route.tsx
+++ app/frames/[projectName]/[strategyName]/route.tsx
@@ -1,58 +1,9 @@
-import { farcasterHubContext } from "frames.js/middleware";
-import { Button, createFrames, types } from "frames.js/next";
+import { Button } from "frames.js/next";
 import { FrameImage } from "../../../_components/FrameImage";
-import yaml from "js-yaml";
-import fs from "fs";
-import { YamlData } from "../../../_types/yamlData";
 import { generateButtonsData } from "../../../_services/buttons";
-import { FrameState } from "../../../_types/frame";
 import { getUpdatedFrameState } from "../../../_services/frameState";
-import path from "path";
-
-const dotrainContext: types.FramesMiddleware<
-  any,
-  { dotrainText: string; yamlData: YamlData }
-> = async (ctx, next) => {
-  const projectName = ctx.url.pathname.split("/")[2];
-  const strategyName = ctx.url.pathname.split("/")[3];
-
-  const filePath = path.join(
-    process.cwd(),
-    "public",
-    "_strategies",
-    projectName,
-    `${strategyName}.rain`
-  );
-  const dotrainText = fs.readFileSync(filePath, "utf8");
-  const yamlData = yaml.load(dotrainText.split("---")[0]) as YamlData;
-
-  return next({ dotrainText, yamlData });
-};
-
-export const frames = createFrames<FrameState>({
-  basePath: "",
-  middleware: [
-    farcasterHubContext({
-      // remove if you aren't using @frames.js/debugger or you just don't want to use the debugger hub
-      ...(process.env.NODE_ENV === "production"
-        ? {}
-        : {
-            hubHttpUrl: "http://localhost:3010/hub",
-          }),
-    }),
-    dotrainContext,
-  ],
-  initialState: {
-    strategyName: null,
-    currentStep: "start",
-    deploymentOption: null,
-    bindings: {},
-    deposit: null,
-    buttonPage: 0,
-    textInputLabel: "",
-    error: null,
-  },
-});
+import { frames } from "./frames";
+import { FrameState } from "@/app/_types/frame";
 
 const parseButtonsData = (buttonsData: any[]) => {
   return buttonsData.map((button) => {
@@ -76,10 +27,11 @@ const parseButtonsData = (buttonsData: any[]) => {
 
 const handleRequest = frames(async (ctx) => {
   const yamlData = ctx.yamlData;
-  if (!ctx.state.strategyName) {
-    ctx.state.strategyName = yamlData.gui.name;
+  let currentState: FrameState = { ...(ctx.state as FrameState) };
+
+  if (currentState && !currentState.strategyName) {
+    currentState.strategyName = yamlData.gui.name;
   }
-  let currentState = { ...ctx.state };
 
   // Handle page navigation
   if (ctx.url.searchParams.has("textInputLabel")) {

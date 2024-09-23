62fadd3be4ae4103f30a9807da50c59bf97c9ad0
Bryan Fullam <bryanfullam@gmail.com>
2024-09-23 13:00:20 +0200

    Remove unused connect button

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { Providers } from "./providers";
-import { ConnectButton } from "@rainbow-me/rainbowkit";
 import "@rainbow-me/rainbowkit/styles.css";
 import "@fontsource/dm-sans/200.css";
 import "@fontsource/dm-sans/300.css";

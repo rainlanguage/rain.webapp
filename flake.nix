dfbedf643e75dbc08377363d40e5fa5f3ac41962
thedavidmeister <thedavidmeister@gmail.com>
2024-09-27 23:28:53 +0400

    fix nix

--- flake.nix
+++ flake.nix
@@ -8,7 +8,9 @@
 
   outputs = { self, flake-utils, rainix }:
     flake-utils.lib.eachDefaultSystem (system:
-      {
+      let
+        pkgs = rainix.pkgs.${system};
+      in rec {
         packages = rec {
           rainframe-test = rainix.mkTask.${system} {
             name = "rainframe-test";
@@ -19,7 +21,16 @@
           };
 
         } // rainix.packages.${system};
-        devShells = rainix.devShells.${system};
+
+        devShells.default = pkgs.mkShell {
+          packages = [
+            packages.rainframe-test
+          ];
+
+          shellHook = rainix.devShells.${system}.default.shellHook;
+          buildInputs = rainix.devShells.${system}.default.buildInputs;
+          nativeBuildInputs = rainix.devShells.${system}.default.nativeBuildInputs;
+        };
       }
     );
 }

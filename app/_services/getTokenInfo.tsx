9d123b37429da8322efca05a711277ed9d3c7c93
Jamie Harding <jharding142@icloud.com>
2024-09-30 14:27:06 +0200

    format

--- app/_services/getTokenInfo.tsx
+++ app/_services/getTokenInfo.tsx
@@ -1,70 +1,65 @@
-import { YamlData } from "../_types/yamlData";
-import { Address, erc20Abi, isHex } from "viem";
-import { getPublicClient } from "./getPublicClient";
+import { YamlData } from '../_types/yamlData';
+import { Address, erc20Abi, isHex } from 'viem';
+import { getPublicClient } from './getPublicClient';
 
 export interface TokenInfo {
-  decimals: number;
-  symbol: string;
-  name: string;
-  yamlName: string;
-  address: Address;
+	decimals: number;
+	symbol: string;
+	name: string;
+	yamlName: string;
+	address: Address;
 }
 
 export const getTokenInfos = async (yaml: YamlData): Promise<TokenInfo[]> => {
-  let tokenInfos = [];
+	let tokenInfos = [];
 
-  for (const [tokenName, token] of Object.entries(yaml.tokens)) {
-    if (!isHex(token.address))
-      throw new Error(
-        `Token address for ${tokenName} is not a hex string: got ${token.address}`
-      );
+	for (const [tokenName, token] of Object.entries(yaml.tokens)) {
+		if (!isHex(token.address))
+			throw new Error(`Token address for ${tokenName} is not a hex string: got ${token.address}`);
 
-    if (!token.network)
-      throw new Error(`Token ${tokenName} does not have a network`);
+		if (!token.network) throw new Error(`Token ${tokenName} does not have a network`);
 
-    const tokenContract = {
-      address: token.address,
-      abi: erc20Abi,
-    } as const;
+		const tokenContract = {
+			address: token.address,
+			abi: erc20Abi
+		} as const;
 
-    const client = getPublicClient(yaml.networks[token.network]);
+		const client = getPublicClient(yaml.networks[token.network]);
 
-    const res = await client.multicall({
-      contracts: [
-        {
-          ...tokenContract,
-          functionName: "decimals",
-        },
-        {
-          ...tokenContract,
-          functionName: "symbol",
-        },
-        {
-          ...tokenContract,
-          functionName: "name",
-        },
-      ],
-    });
+		const res = await client.multicall({
+			contracts: [
+				{
+					...tokenContract,
+					functionName: 'decimals'
+				},
+				{
+					...tokenContract,
+					functionName: 'symbol'
+				},
+				{
+					...tokenContract,
+					functionName: 'name'
+				}
+			]
+		});
 
-    if (res.some((r) => r.status !== "success")) {
-      throw new Error(
-        `Failed to fetch token info for ${tokenName}: ${res
-          .map((r) => r.error)
-          .join(", ")}`
-      );
-    }
+		if (res.some((r) => r.status !== 'success')) {
+			throw new Error(
+				`Failed to fetch token info for ${tokenName}: ${res.map((r) => r.error).join(', ')}`
+			);
+		}
 
-    if (!res[0].result) throw new Error(`Token ${tokenName} has no decimals`);
-    if (!res[1].result) throw new Error(`Token ${tokenName} has no symbol`);
-    if (!res[2].result) throw new Error(`Token ${tokenName} has no name`);
+		if (!res[0].result) throw new Error(`Token ${tokenName} has no decimals`);
+		if (!res[1].result) throw new Error(`Token ${tokenName} has no symbol`);
+		if (!res[2].result) throw new Error(`Token ${tokenName} has no name`);
 
-    tokenInfos.push({
-      yamlName: tokenName,
-      address: token.address,
-      decimals: res[0].result,
-      symbol: res[1].result,
-      name: res[2].result,
-    });
-  }
-  return tokenInfos;
+		tokenInfos.push({
+			yamlName: tokenName,
+			address: token.address,
+			decimals: res[0].result,
+			symbol: res[1].result,
+			name: res[2].result
+		});
+	}
+	return tokenInfos;
 };
--- app/frames/[projectName]/[strategyName]/[deployment]/frames.ts
+++ app/frames/[projectName]/[strategyName]/[deployment]/frames.ts
@@ -1,72 +1,68 @@
-import { YamlData } from "@/app/_types/yamlData";
-import { farcasterHubContext } from "frames.js/middleware";
-import { createFrames, types } from "frames.js/next";
-import { FrameState } from "frames.js/next/types";
-import path from "path";
-import yaml from "js-yaml";
-import fs from "fs";
-import { FailsafeSchemaWithNumbers } from "@/app/_schemas/failsafeWithNumbers";
+import { YamlData } from '@/app/_types/yamlData';
+import { farcasterHubContext } from 'frames.js/middleware';
+import { createFrames, types } from 'frames.js/next';
+import { FrameState } from 'frames.js/next/types';
+import path from 'path';
+import yaml from 'js-yaml';
+import fs from 'fs';
+import { FailsafeSchemaWithNumbers } from '@/app/_schemas/failsafeWithNumbers';
 
 const dotrainContext: types.FramesMiddleware<
-  any,
-  { dotrainText: string; yamlData: YamlData }
+	any,
+	{ dotrainText: string; yamlData: YamlData }
 > = async (ctx, next) => {
-  const projectName = ctx.url.pathname.split("/")[2];
-  const strategyName = ctx.url.pathname.split("/")[3];
+	const projectName = ctx.url.pathname.split('/')[2];
+	const strategyName = ctx.url.pathname.split('/')[3];
 
-  const allDirs = fs.readdirSync(
-    path.join(process.cwd(), "public", "_strategies", projectName)
-  );
+	const allDirs = fs.readdirSync(path.join(process.cwd(), 'public', '_strategies', projectName));
 
-  const strategyDir = allDirs.find((dir) => dir.endsWith(strategyName));
+	const strategyDir = allDirs.find((dir) => dir.endsWith(strategyName));
 
-  if (!strategyDir) {
-    throw new Error(
-      `No directory found for strategy: ${strategyName} in project: ${projectName}`
-    );
-  }
+	if (!strategyDir) {
+		throw new Error(`No directory found for strategy: ${strategyName} in project: ${projectName}`);
+	}
 
-  const filePath = path.join(
-    process.cwd(),
-    "public",
-    "_strategies",
-    projectName,
-    strategyDir,
-    `${strategyName}.rain`
-  );
-  const dotrainText = fs.readFileSync(filePath, "utf8");
-  const yamlData = yaml.load(dotrainText.split("---")[0], {
-    schema: FailsafeSchemaWithNumbers,
-  }) as YamlData;
+	const filePath = path.join(
+		process.cwd(),
+		'public',
+		'_strategies',
+		projectName,
+		strategyDir,
+		`${strategyName}.rain`
+	);
+	const dotrainText = fs.readFileSync(filePath, 'utf8');
+	const yamlData = yaml.load(dotrainText.split('---')[0], {
+		schema: FailsafeSchemaWithNumbers
+	}) as YamlData;
 
-  return next({ dotrainText, yamlData });
+	return next({ dotrainText, yamlData });
 };
 
 export const frames = createFrames<FrameState>({
-  basePath: "",
-  middleware: [
-    farcasterHubContext({
-      // remove if you aren't using @frames.js/debugger or you just don't want to use the debugger hub
-      ...(process.env.NODE_ENV === "production"
-        ? {}
-        : {
-            hubHttpUrl: "http://localhost:3010/hub",
-          }),
-    }),
-    dotrainContext,
-  ],
-  initialState: {
-    strategyName: null,
-    strategyDescription: null,
-    currentStep: "start",
-    deploymentOption: null,
-    bindings: {},
-    deposits: [],
-    buttonPage: 0,
-    textInputLabel: "",
-    error: null,
-    requiresTokenApproval: true,
-    tokensApproved: false,
-    tokenInfos: [],
-  },
+	basePath: '',
+	middleware: [
+		farcasterHubContext({
+			// remove if you aren't using @frames.js/debugger or you just don't want to use the debugger hub
+			...(process.env.NODE_ENV === 'production'
+				? {}
+				: {
+						hubHttpUrl: 'http://localhost:3010/hub'
+					})
+		}),
+		dotrainContext
+	],
+	initialState: {
+		strategyName: null,
+		strategyDescription: null,
+		currentStep: 'start',
+		deploymentOption: null,
+		bindings: {},
+		deposits: [],
+		buttonPage: 0,
+		textInputLabel: '',
+		error: null,
+		requiresTokenApproval: true,
+		tokensApproved: false,
+		tokenInfos: []
+	}
 });

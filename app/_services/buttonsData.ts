161e842089d08ae44a66f33981c3a9bbb6d229aa
Jamie Harding <jharding142@icloud.com>
2024-09-30 12:12:26 +0200

    add

--- app/_services/buttonsData.ts
+++ app/_services/buttonsData.ts
@@ -7,7 +7,7 @@ export const getPaginatedButtons = (
 	allButtons: Button[],
 	buttonPage: number,
 	buttonMax = 4
-): any[] => {
+): unknown[] => {
 	console.log('all buttons', allButtons);
 	const buttonPageOffset = buttonPage * 3;
 	let buttonEndIndex = buttonPageOffset + buttonMax;

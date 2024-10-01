71dafb69c04f73ee2b74c0e412991569c92e455e
Jamie Harding <jharding142@icloud.com>
2024-10-01 10:46:23 +0200

    const

--- eslint.config.js
+++ eslint.config.js
@@ -57,10 +57,9 @@ export default [
 			'@typescript-eslint/no-unused-vars': 'off',
 			'@typescript-eslint/no-explicit-any': 'off',
 			'@typescript-eslint/no-empty-object-type': 'off',
-			'no-case-declarations': 'off'
-			// 'react/no-unescaped-entities': 'off',
+			'no-case-declarations': 'off',
+			'react/no-unescaped-entities': 'off',
 			// 'no-unsafe-optional-chaining': 'off',
-			// 'prefer-const': 'off',
 		}
 	}
 ];

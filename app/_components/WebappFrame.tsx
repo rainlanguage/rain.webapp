bf7a4bdd0583680548fbd0a3b41d218750beef88
Jamie Harding <jharding142@icloud.com>
2024-10-02 12:09:06 +0200

    some changes

--- app/_components/WebappFrame.tsx
+++ app/_components/WebappFrame.tsx
@@ -65,6 +65,7 @@ const WebappFrame = ({ dotrainText, deploymentOption }: props) => {
 		fetchingTokens: false,
 		decodingState: true
 	});
+
 	const [error, setError] = useState<string | React.ReactElement | null>(null);
 	const [inputText, setInputText] = useState<string>('');
 
@@ -149,10 +150,14 @@ const WebappFrame = ({ dotrainText, deploymentOption }: props) => {
 			}));
 			return;
 		} else if (buttonData.buttonTarget === 'buttonValue' && buttonData.buttonValue === 'back') {
+			console.log('going back', buttonData);
+
 			setCurrentState((prevState) => ({
 				...prevState,
 				textInputLabel: ''
 			}));
+
+			
 		}
 
 		const updatedState = getUpdatedFrameState(
@@ -171,23 +176,21 @@ const WebappFrame = ({ dotrainText, deploymentOption }: props) => {
 
 	const buttonsData = generateButtonsData(yamlData, currentState);
 	useEffect(() => {
-		console.log(buttonsData);
-		console.log('state',currentState);
-
+		console.log('buttons data', buttonsData);
+		console.log('state', currentState);
 	}, [buttonsData]);
 
-	useEffect(() => {
-		console.log(buttonsData);
-		const filteredButtons = buttonsData.filter(
-			(buttonData) => buttonData.buttonValue !== 'back' && buttonData.buttonValue !== 'finalSubmit'
-		);
-		if (filteredButtons.length === 1 && filteredButtons[0].buttonText === 'Custom') {
-			setCurrentState((prevState) => ({
-				...prevState,
-				textInputLabel: filteredButtons[0].buttonValue
-			}));
-		}
-	}, [buttonsData]);
+	// useEffect(() => {
+	// 	const filteredButtons = buttonsData.filter(
+	// 		(buttonData) => buttonData.buttonValue !== 'back' && buttonData.buttonValue !== 'finalSubmit'
+	// 	);
+	// 	if (filteredButtons.length === 1 && filteredButtons[0].buttonText === 'Custom') {
+	// 		setCurrentState((prevState) => ({
+	// 			...prevState,
+	// 			textInputLabel: filteredButtons[0].buttonValue
+	// 		}));
+	// 	}
+	// }, [buttonsData]);
 
 	return loading.decodingState || loading.fetchingTokens ? (
 		<div className="flex-grow flex items-center justify-center">

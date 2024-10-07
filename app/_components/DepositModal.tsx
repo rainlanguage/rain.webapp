b3a95856ef9647257226c0a1396e97e6d3fe2f59
Jamie Harding <jharding142@icloud.com>
2024-10-07 21:17:11 +0200

    format

--- app/_components/DepositModal.tsx
+++ app/_components/DepositModal.tsx
@@ -228,7 +228,8 @@ export const DepositModal = ({ vault }: DepositModalProps) => {
 					className={cn(
 						buttonVariants(),
 						'bg-blue-500 hover:bg-blue-700 text-white py-2 px-4 rounded-xl transition-colors cursor-pointer'
-					)}>
+					)}
+				>
 					Deposit
 				</span>
 			</DialogTrigger>
@@ -241,7 +242,8 @@ export const DepositModal = ({ vault }: DepositModalProps) => {
 								onSubmit={form.handleSubmit(async () => {
 									await deposit();
 								})}
-								className="space-y-8">
+								className="space-y-8"
+							>
 								<FormField
 									control={form.control}
 									name="depositAmount"
@@ -305,7 +307,8 @@ export const DepositModal = ({ vault }: DepositModalProps) => {
 										<a
 											href={(chain?.blockExplorers.default.url as string) + '/tx/' + depositTxHash}
 											target="_blank"
-											rel="noreferrer">
+											rel="noreferrer"
+										>
 											<Button className="w-fit">View Transaction</Button>
 										</a>
 									)}
@@ -326,7 +329,8 @@ export const DepositModal = ({ vault }: DepositModalProps) => {
 													  depositState === TokenDepositStatus.WaitingForApprovalConfirmation
 													? 'bg-amber-500 w-12 h-12'
 													: 'bg-emerald-600 w-10 h-10'
-										}`}>
+										}`}
+									>
 										{1}
 									</div>
 									<div className="text-lg">
@@ -358,7 +362,8 @@ export const DepositModal = ({ vault }: DepositModalProps) => {
 													: depositState === TokenDepositStatus.TokensDeposited
 														? 'bg-emerald-600 w-10 h-10'
 														: 'bg-gray-400 w-10 h-10'
-										}`}>
+										}`}
+									>
 										{2}
 									</div>
 									<div className="text-lg">
@@ -385,7 +390,8 @@ export const DepositModal = ({ vault }: DepositModalProps) => {
 								<a
 									href={(chain?.blockExplorers.default.url as string) + '/tx/' + depositTxHash}
 									target="_blank"
-									rel="noreferrer">
+									rel="noreferrer"
+								>
 									<Button className="w-fit">View Transaction</Button>
 								</a>
 							)}

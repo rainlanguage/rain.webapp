a1163fd482e0c427eae4771b1cdd9741553abbbf
Jamie Harding <jharding142@icloud.com>
2024-10-07 17:01:19 +0200

    setup

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,7 +3,14 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
 	plugins: [react()],
+
 	test: {
-		environment: 'jsdom'
+		// Jest like globals
+		includeSource: ['src/**/*.{js,ts}'],
+		globals: true,
+		environment: 'jsdom',
+		include: ['src/**/*.{test,spec}.ts'],
+		// Extend jest-dom matchers
+		setupFiles: ['./setupTest.ts']
 	}
 });

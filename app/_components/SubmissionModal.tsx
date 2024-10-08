75a6459bdf319bdc8cf463bf330d42742776f708
Jamie Harding <jharding142@icloud.com>
2024-10-08 17:06:23 +0200

    merge

--- app/_components/SubmissionModal.tsx
+++ app/_components/SubmissionModal.tsx
@@ -156,8 +156,7 @@ export const SubmissionModal = ({
 													href={ref.url}
 													target="_blank"
 													rel="noreferrer"
-													style={{ color: 'blue', textDecoration: 'underline' }}
-												>
+													style={{ color: 'blue', textDecoration: 'underline' }}>
 													{ref.name}
 												</a>
 											</li>
@@ -304,8 +303,7 @@ export const SubmissionModal = ({
 					onClick={() => setOpen(true)}
 					color="primary"
 					size="sm"
-					className=" from-blue-600 to-violet-600 bg-gradient-to-br"
-				>
+					className=" from-blue-600 to-violet-600 bg-gradient-to-br">
 					{buttonText}
 				</Button>
 			) : (
@@ -313,8 +311,7 @@ export const SubmissionModal = ({
 			)}
 			<DialogContent
 				onInteractOutside={resetSubmissionState}
-				className="bg-white flex flex-col justify-center w-full font-light gap-y-8"
-			>
+				className="bg-white flex flex-col justify-center w-full font-light gap-y-8">
 				{showDisclaimer && (
 					<div className="flex flex-col items-start gap-y-4">
 						<DialogTitle className="w-full font-light text-2xl">Wait!</DialogTitle>
@@ -354,8 +351,7 @@ export const SubmissionModal = ({
 							onClick={() => {
 								setShowDisclaimer(false);
 								submitStrategy();
-							}}
-						>
+							}}>
 							I understand
 						</Button>
 					</div>
@@ -369,8 +365,7 @@ export const SubmissionModal = ({
 						<div
 							className={`transition-opacity duration-1000 flex flex-col ${
 								submissionState === SubmissionStatus.Done ? 'opacity-0' : 'opacity-100'
-							}`}
-						>
+							}`}>
 							{tokenDeposits.map((deposit, i) => (
 								<div key={i} className="flex items-center my-4">
 									<div
@@ -382,8 +377,7 @@ export const SubmissionModal = ({
 													  deposit.status === TokenDepositStatus.WaitingForApprovalConfirmation
 													? 'bg-amber-500 w-12 h-12'
 													: 'bg-emerald-600 w-10 h-10'
-										}`}
-									>
+										}`}>
 										{i + 1}
 									</div>
 									<div className="text-lg">
@@ -416,8 +410,7 @@ export const SubmissionModal = ({
 												: submissionState === SubmissionStatus.Done
 													? 'bg-emerald-600 w-10 h-10'
 													: 'bg-gray-400 w-10 h-10'
-									}`}
-								>
+									}`}>
 									{tokenDeposits.length + 1}
 								</div>
 								<div className="text-lg">
@@ -444,8 +437,7 @@ export const SubmissionModal = ({
 						</div>
 						<Button
 							className="mt-4"
-							onClick={() => router.push(`${window.location.origin}/my-strategies`)}
-						>
+							onClick={() => router.push(`${window.location.origin}/my-strategies`)}>
 							Track your strategy
 						</Button>
 					</div>

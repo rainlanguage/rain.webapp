0882075c1dca76a6cdee6ca17bfbaf445e666e60
Jamie Harding <jharding142@icloud.com>
2024-10-10 15:51:57 +0200

    format

--- app/_components/DepositModal.tsx
+++ app/_components/DepositModal.tsx
@@ -229,7 +229,8 @@ export const DepositModal = ({ vault, networkStatus }: DepositModalProps) => {
 					className={cn(
 						buttonVariants(),
 						'bg-blue-500 hover:bg-blue-700 text-white py-2 px-4 rounded-xl transition-colors cursor-pointer'
-					)}>
+					)}
+				>
 					Deposit
 				</span>
 			</DialogTrigger>
@@ -242,7 +243,8 @@ export const DepositModal = ({ vault, networkStatus }: DepositModalProps) => {
 								onSubmit={form.handleSubmit(async () => {
 									await deposit();
 								})}
-								className="space-y-2">
+								className="space-y-2"
+							>
 								<FormField
 									control={form.control}
 									name="depositAmount"
@@ -306,7 +308,8 @@ export const DepositModal = ({ vault, networkStatus }: DepositModalProps) => {
 										<a
 											href={(chain?.blockExplorers.default.url as string) + '/tx/' + depositTxHash}
 											target="_blank"
-											rel="noreferrer">
+											rel="noreferrer"
+										>
 											<Button className="w-fit">View Transaction</Button>
 										</a>
 									)}
@@ -327,7 +330,8 @@ export const DepositModal = ({ vault, networkStatus }: DepositModalProps) => {
 													  depositState === TokenDepositStatus.WaitingForApprovalConfirmation
 													? 'bg-amber-500 w-12 h-12'
 													: 'bg-emerald-600 w-10 h-10'
-										}`}>
+										}`}
+									>
 										{1}
 									</div>
 									<div className="text-lg">
@@ -359,7 +363,8 @@ export const DepositModal = ({ vault, networkStatus }: DepositModalProps) => {
 													: depositState === TokenDepositStatus.TokensDeposited
 														? 'bg-emerald-600 w-10 h-10'
 														: 'bg-gray-400 w-10 h-10'
-										}`}>
+										}`}
+									>
 										{2}
 									</div>
 									<div className="text-lg">
@@ -386,7 +391,8 @@ export const DepositModal = ({ vault, networkStatus }: DepositModalProps) => {
 								<a
 									href={(chain?.blockExplorers.default.url as string) + '/tx/' + depositTxHash}
 									target="_blank"
-									rel="noreferrer">
+									rel="noreferrer"
+								>
 									<Button className="w-fit">View Transaction</Button>
 								</a>
 							)}
--- app/_components/WithdrawalModal.tsx
+++ app/_components/WithdrawalModal.tsx
@@ -115,7 +115,8 @@ export const WithdrawalModal = ({ vault, networkStatus }: WithdrawalModalProps) 
 					className={cn(
 						buttonVariants(),
 						'bg-blue-500 hover:bg-blue-700 text-white py-2 px-4 rounded-xl transition-colors cursor-pointer'
-					)}>
+					)}
+				>
 					Withdraw
 				</span>
 			</DialogTrigger>
@@ -129,7 +130,8 @@ export const WithdrawalModal = ({ vault, networkStatus }: WithdrawalModalProps) 
 								await withdraw(rawAmount);
 								setOpen(false);
 							})}
-							className="space-y-2">
+							className="space-y-2"
+						>
 							<FormField
 								control={form.control}
 								name="withdrawalAmount"

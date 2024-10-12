2cbc329ff556f6ca30c73262fe3ec1d6acb90f0e
thedavidmeister <thedavidmeister@gmail.com>
2024-10-12 10:52:43 +0400

    rollback flare sg

--- app/_queries/subgraphs.tsx
+++ app/_queries/subgraphs.tsx
@@ -1,7 +1,7 @@
 export const getNetworkSubgraphs = () => {
 	return {
 		flare:
-			'https://api.goldsky.com/api/public/project_clv14x04y9kzi01saerx7bxpg/subgraphs/ob4-flare/0.6/gn',
+			'https://api.goldsky.com/api/public/project_clv14x04y9kzi01saerx7bxpg/subgraphs/ob4-flare/0.2/gn',
 		base: 'https://api.goldsky.com/api/public/project_clv14x04y9kzi01saerx7bxpg/subgraphs/ob4-base/0.7/gn',
 		linea:
 			'https://api.goldsky.com/api/public/project_clv14x04y9kzi01saerx7bxpg/subgraphs/ob4-linea/0.1/gn',

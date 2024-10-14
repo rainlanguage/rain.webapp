094711ccd851147f370b44c21b6807e1b1502779
Jamie Harding <jharding142@icloud.com>
2024-10-14 17:52:37 +0200

    remove

--- app/_services/dates.ts
+++ app/_services/dates.ts
@@ -1,11 +1,3 @@
-import dayjs from 'dayjs';
-import bigIntSupport from 'dayjs/plugin/bigIntSupport';
-import localizedFormat from 'dayjs/plugin/localizedFormat';
-
-dayjs.extend(bigIntSupport);
-dayjs.extend(localizedFormat);
-
 export function formatTimestampSecondsAsLocal(timestampSeconds: bigint) {
-	const date = dayjs(timestampSeconds * BigInt('1000'));
-	return date.toDate().toLocaleString(navigator.language || undefined, {});
+	return new Date(Number(timestampSeconds) * 1000).toLocaleString();
 }

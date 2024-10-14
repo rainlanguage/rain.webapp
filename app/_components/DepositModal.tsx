9288c57400600c936b22d52330d8a726533c1876
Jamie Harding <jharding142@icloud.com>
2024-10-14 16:12:34 +0200

    add error

--- app/_components/DepositModal.tsx
+++ app/_components/DepositModal.tsx
@@ -242,7 +242,8 @@ export const DepositModal = ({ vault, network }: DepositModalProps) => {
 					className={cn(
 						buttonVariants(),
 						'bg-blue-500 hover:bg-blue-700 text-white py-2 px-4 rounded-xl transition-colors cursor-pointer'
-					)}>
+					)}
+				>
 					Deposit
 				</span>
 			</DialogTrigger>
@@ -255,7 +256,8 @@ export const DepositModal = ({ vault, network }: DepositModalProps) => {
 								onSubmit={form.handleSubmit(async () => {
 									await deposit();
 								})}
-								className="space-y-8">
+								className="space-y-8"
+							>
 								<FormField
 									control={form.control}
 									name="depositAmount"
@@ -319,7 +321,8 @@ export const DepositModal = ({ vault, network }: DepositModalProps) => {
 										<a
 											href={(chain?.blockExplorers.default.url as string) + '/tx/' + depositTxHash}
 											target="_blank"
-											rel="noreferrer">
+											rel="noreferrer"
+										>
 											<Button className="w-fit">View Transaction</Button>
 										</a>
 									)}
@@ -340,7 +343,8 @@ export const DepositModal = ({ vault, network }: DepositModalProps) => {
 													  depositState === TokenDepositStatus.WaitingForApprovalConfirmation
 													? 'bg-amber-500 w-12 h-12'
 													: 'bg-emerald-600 w-10 h-10'
-										}`}>
+										}`}
+									>
 										{1}
 									</div>
 									<div className="text-lg">
@@ -372,7 +376,8 @@ export const DepositModal = ({ vault, network }: DepositModalProps) => {
 													: depositState === TokenDepositStatus.TokensDeposited
 														? 'bg-emerald-600 w-10 h-10'
 														: 'bg-gray-400 w-10 h-10'
-										}`}>
+										}`}
+									>
 										{2}
 									</div>
 									<div className="text-lg">
@@ -399,7 +404,8 @@ export const DepositModal = ({ vault, network }: DepositModalProps) => {
 								<a
 									href={(chain?.blockExplorers.default.url as string) + '/tx/' + depositTxHash}
 									target="_blank"
-									rel="noreferrer">
+									rel="noreferrer"
+								>
 									<Button className="w-fit">View Transaction</Button>
 								</a>
 							)}

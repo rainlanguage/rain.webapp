0d226530479e608a67b2e3ee9fde6609aca0e1e4
Jamie Harding <jharding142@icloud.com>
2024-10-14 17:55:47 +0200

    tests

--- app/_services/dates.ts
+++ app/_services/dates.ts
@@ -1,3 +1,5 @@
-export function formatTimestampSecondsAsLocal(timestampSeconds: bigint) {
-	return new Date(Number(timestampSeconds) * 1000).toLocaleString();
+export function formatTimestampSecondsAsLocal(timestampSeconds: bigint, locale?: string) {
+	const selectedLocale =
+		locale || (typeof navigator !== 'undefined' ? navigator.language : 'en-US');
+	return new Date(Number(timestampSeconds) * 1000).toLocaleString(selectedLocale);
 }

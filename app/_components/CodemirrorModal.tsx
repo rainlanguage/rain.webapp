40b6f2a9749bf22087fc113331e5abca806bdc8e
Jamie Harding <jharding142@icloud.com>
2024-10-16 16:28:10 +0200

    format

--- app/_components/CodemirrorModal.tsx
+++ app/_components/CodemirrorModal.tsx
@@ -38,7 +38,8 @@ export const CodemirrorModal = ({ currentState, dotrainText }: CodemirrorModalPr
 				<Button
 					color="primary"
 					size="sm"
-					className=" from-blue-600 to-violet-600 bg-gradient-to-br">
+					className=" from-blue-600 to-violet-600 bg-gradient-to-br"
+				>
 					Show Rainlang
 				</Button>
 			</DialogTrigger>

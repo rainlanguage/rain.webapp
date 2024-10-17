a7f4e0d6409f1fa28831d61450e934f68ae8810b
Jamie Harding <jharding142@icloud.com>
2024-10-17 11:14:11 +0200

    refetch and test

--- app/_components/DepositModal.tsx
+++ app/_components/DepositModal.tsx
@@ -102,20 +102,13 @@ export const DepositModal = ({ vault, network, onSuccess }: DepositModalProps) =
 		}
 	};
 
-	const handleDismiss = () => {
-		setOpen(false);
-		setDepositTxHash(null);
-		setDepositState(TokenDepositStatus.Idle);
-		setError(null);
-	};
-
-	const connectedWalletBalance: bigint = useReadContract({
+	const { data: connectedWalletBalance, refetch: refetchBalance } = useReadContract({
 		abi: ERC20_ABI,
 		address: vault.token.address as `0x${string}`,
 		functionName: 'balanceOf',
 		args: [address as `0x${string}`],
 		chainId: chain.id as (typeof config.chains)[number]['id']
-	}).data as bigint;
+	}) as { data: bigint | undefined; refetch: () => void };
 
 	const form = useForm<z.infer<typeof formSchema>>({
 		resolver: zodResolver(formSchema),
@@ -132,7 +125,7 @@ export const DepositModal = ({ vault, network, onSuccess }: DepositModalProps) =
 			Number(vault.token.decimals)
 		).toString();
 		setRawAmount(parsedRawAmount);
-		if (BigInt(parsedRawAmount) > connectedWalletBalance) {
+		if (BigInt(parsedRawAmount) > Number(connectedWalletBalance)) {
 			setError('Amount exceeds wallet balance');
 		} else {
 			setError(null);
@@ -209,6 +202,7 @@ export const DepositModal = ({ vault, network, onSuccess }: DepositModalProps) =
 			});
 
 			setDepositState(TokenDepositStatus.Done);
+			refetchBalance?.();
 			onSuccess?.();
 		} catch (error: unknown) {
 			setDepositState(TokenDepositStatus.Error);
@@ -240,6 +234,13 @@ export const DepositModal = ({ vault, network, onSuccess }: DepositModalProps) =
 		if (address) setOpen(open);
 	};
 
+	const handleDismiss = () => {
+		setOpen(false);
+		setDepositTxHash(null);
+		setDepositState(TokenDepositStatus.Idle);
+		setError(null);
+	};
+
 	return (
 		<Dialog open={open} onOpenChange={connect}>
 			<DialogTrigger>
@@ -247,8 +248,7 @@ export const DepositModal = ({ vault, network, onSuccess }: DepositModalProps) =
 					className={cn(
 						buttonVariants(),
 						'bg-blue-500 hover:bg-blue-700 text-white py-2 px-4 rounded-xl transition-colors cursor-pointer'
-					)}
-				>
+					)}>
 					Deposit
 				</span>
 			</DialogTrigger>
@@ -261,8 +261,7 @@ export const DepositModal = ({ vault, network, onSuccess }: DepositModalProps) =
 								onSubmit={form.handleSubmit(async () => {
 									await deposit();
 								})}
-								className="space-y-8"
-							>
+								className="space-y-8">
 								<FormField
 									control={form.control}
 									name="depositAmount"
@@ -326,8 +325,7 @@ export const DepositModal = ({ vault, network, onSuccess }: DepositModalProps) =
 										<a
 											href={(chain?.blockExplorers.default.url as string) + '/tx/' + depositTxHash}
 											target="_blank"
-											rel="noreferrer"
-										>
+											rel="noreferrer">
 											<Button className="w-fit">View Transaction</Button>
 										</a>
 									)}
@@ -348,8 +346,7 @@ export const DepositModal = ({ vault, network, onSuccess }: DepositModalProps) =
 													  depositState === TokenDepositStatus.WaitingForApprovalConfirmation
 													? 'bg-amber-500 w-12 h-12'
 													: 'bg-emerald-600 w-10 h-10'
-										}`}
-									>
+										}`}>
 										{1}
 									</div>
 									<div className="text-lg">
@@ -381,8 +378,7 @@ export const DepositModal = ({ vault, network, onSuccess }: DepositModalProps) =
 													: depositState === TokenDepositStatus.TokensDeposited
 														? 'bg-emerald-600 w-10 h-10'
 														: 'bg-gray-400 w-10 h-10'
-										}`}
-									>
+										}`}>
 										{2}
 									</div>
 									<div className="text-lg">
@@ -409,8 +405,7 @@ export const DepositModal = ({ vault, network, onSuccess }: DepositModalProps) =
 								<a
 									href={(chain?.blockExplorers.default.url as string) + '/tx/' + depositTxHash}
 									target="_blank"
-									rel="noreferrer"
-								>
+									rel="noreferrer">
 									<Button className="w-fit">View Transaction</Button>
 								</a>
 							)}

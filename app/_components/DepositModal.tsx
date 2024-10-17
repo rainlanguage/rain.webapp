44e45272db445c56330462cc321f514af5fe8f28
Jamie Harding <jharding142@icloud.com>
2024-10-17 14:58:31 +0200

    format

--- app/_components/DepositModal.tsx
+++ app/_components/DepositModal.tsx
@@ -247,7 +247,8 @@ export const DepositModal = ({ vault, network, onSuccess }: DepositModalProps) =
 					className={cn(
 						buttonVariants(),
 						'bg-blue-500 hover:bg-blue-700 text-white py-2 px-4 rounded-xl transition-colors cursor-pointer'
-					)}>
+					)}
+				>
 					Deposit
 				</span>
 			</DialogTrigger>
@@ -260,7 +261,8 @@ export const DepositModal = ({ vault, network, onSuccess }: DepositModalProps) =
 								onSubmit={form.handleSubmit(async () => {
 									await deposit();
 								})}
-								className="space-y-8">
+								className="space-y-8"
+							>
 								<FormField
 									control={form.control}
 									name="depositAmount"
@@ -324,7 +326,8 @@ export const DepositModal = ({ vault, network, onSuccess }: DepositModalProps) =
 										<a
 											href={(chain?.blockExplorers.default.url as string) + '/tx/' + depositTxHash}
 											target="_blank"
-											rel="noreferrer">
+											rel="noreferrer"
+										>
 											<Button className="w-fit">View Transaction</Button>
 										</a>
 									)}
@@ -345,7 +348,8 @@ export const DepositModal = ({ vault, network, onSuccess }: DepositModalProps) =
 													  depositState === TokenDepositStatus.WaitingForApprovalConfirmation
 													? 'bg-amber-500 w-12 h-12'
 													: 'bg-emerald-600 w-10 h-10'
-										}`}>
+										}`}
+									>
 										{1}
 									</div>
 									<div className="text-lg">
@@ -377,7 +381,8 @@ export const DepositModal = ({ vault, network, onSuccess }: DepositModalProps) =
 													: depositState === TokenDepositStatus.TokensDeposited
 														? 'bg-emerald-600 w-10 h-10'
 														: 'bg-gray-400 w-10 h-10'
-										}`}>
+										}`}
+									>
 										{2}
 									</div>
 									<div className="text-lg">
@@ -404,7 +409,8 @@ export const DepositModal = ({ vault, network, onSuccess }: DepositModalProps) =
 								<a
 									href={(chain?.blockExplorers.default.url as string) + '/tx/' + depositTxHash}
 									target="_blank"
-									rel="noreferrer">
+									rel="noreferrer"
+								>
 									<Button className="w-fit">View Transaction</Button>
 								</a>
 							)}

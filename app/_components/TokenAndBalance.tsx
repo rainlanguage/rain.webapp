c7997d21c213a113d1b39078d2fd615048167fdf
Jamie Harding <jharding142@icloud.com>
2024-10-18 11:23:45 +0200

    do not round

--- app/_components/TokenAndBalance.tsx
+++ app/_components/TokenAndBalance.tsx
@@ -19,13 +19,11 @@ export function TokenAndBalance({
 	return (
 		<div
 			className="flex border rounded-xl p-2 gap-x-3 items-center justify-between"
-			data-testid="token-balance"
-		>
+			data-testid="token-balance">
 			<div className="flex flex-col gap-y-1">
 				<div>{withdraw ? input.token.name : input.token.symbol}</div>
 				<div className="text-gray-500">
-					Strategy Balance:{' '}
-					{Number(Number(formatUnits(input.balance, Number(input.token.decimals))).toFixed(8))}
+					Strategy Balance: {formatUnits(input.balance, Number(input.token.decimals))}
 				</div>
 			</div>
 			<div className="flex gap-2">

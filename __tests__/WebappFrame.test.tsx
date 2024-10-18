2a37a38646513e8103faacd994f8f7ac2afa5c14
Jamie Harding <jharding142@icloud.com>
2024-10-18 11:55:29 +0200

    format

--- __tests__/WebappFrame.test.tsx
+++ __tests__/WebappFrame.test.tsx
@@ -6,27 +6,27 @@ import { vi } from 'vitest';
 
 // Mock next/navigation
 vi.mock('next/navigation', () => ({
-  useSearchParams: () => new URLSearchParams({}),
+	useSearchParams: () => new URLSearchParams({})
 }));
 
 // Mock getTokenInfo
 vi.mock('@/app/_services/getTokenInfo', () => ({
-  getTokenInfos: vi.fn().mockResolvedValue([
-    {
-      decimals: 18,
-      symbol: 'WETH',
-      name: 'Wrapped Ether',
-      yamlName: 'weth',
-      address: '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2'
-    },
-    {
-      decimals: 6,
-      symbol: 'USDC',
-      name: 'USD Coin',
-      yamlName: 'usdc',
-      address: '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48'
-    }
-  ]),
+	getTokenInfos: vi.fn().mockResolvedValue([
+		{
+			decimals: 18,
+			symbol: 'WETH',
+			name: 'Wrapped Ether',
+			yamlName: 'weth',
+			address: '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2'
+		},
+		{
+			decimals: 6,
+			symbol: 'USDC',
+			name: 'USD Coin',
+			yamlName: 'usdc',
+			address: '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48'
+		}
+	])
 }));
 
 describe('WebappFrame', () => {
@@ -34,22 +34,22 @@ describe('WebappFrame', () => {
 		const filePath = path.join(
 			process.cwd(),
 			'public',
-				'_strategies',
-				'raindex',
-				'2-dynamic-spread',
-				'dynamic-spread.rain'
+			'_strategies',
+			'raindex',
+			'2-dynamic-spread',
+			'dynamic-spread.rain'
 		);
 		const dotrainText = fs.readFileSync(filePath, 'utf8');
 
-        render(<WebappFrame dotrainText={dotrainText} deploymentOption={null} />);
+		render(<WebappFrame dotrainText={dotrainText} deploymentOption={null} />);
 
-        await waitFor(() => {
-            expect(screen.getByText(/Start/i)).toBeInTheDocument();
-        });
-        fireEvent.click(screen.getByText(/Start/i));
+		await waitFor(() => {
+			expect(screen.getByText(/Start/i)).toBeInTheDocument();
+		});
+		fireEvent.click(screen.getByText(/Start/i));
 
-        await waitFor(() => {
-            expect(screen.getByText(/Initial price (USDC.e per WETH)/i)).toBeInTheDocument();
-        });
+		await waitFor(() => {
+			expect(screen.getByText(/Initial price (USDC.e per WETH)/i)).toBeInTheDocument();
+		});
 	});
 });

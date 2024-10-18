3f79bfd8e31620da31c89d4a5b8937cae4a12fa4
Jamie Harding <jharding142@icloud.com>
2024-10-18 16:13:34 +0200

    format

--- app/_components/WebappFrame.tsx
+++ app/_components/WebappFrame.tsx
@@ -214,7 +214,8 @@ const WebappFrame = ({ dotrainText, deploymentOption }: props) => {
 							key={buttonData.buttonText}
 							onClick={async () => {
 								await handleButtonClick(buttonData);
-							}}>
+							}}
+						>
 							{buttonData.buttonText}
 						</Button>
 					);
@@ -236,7 +237,8 @@ const WebappFrame = ({ dotrainText, deploymentOption }: props) => {
 					<DialogClose asChild>
 						<button
 							className="bg-blue-500 hover:bg-blue-700 text-white py-2 px-4 rounded-xl transition-colors"
-							onClick={() => setError(null)}>
+							onClick={() => setError(null)}
+						>
 							Close
 						</button>
 					</DialogClose>

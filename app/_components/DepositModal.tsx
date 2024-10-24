0eff1833f4bef8ac8538fb42491a8fcbe7849686
Jamie Harding <jharding142@icloud.com>
2024-10-24 18:37:31 +0200

    format

--- app/_components/DepositModal.tsx
+++ app/_components/DepositModal.tsx
@@ -247,7 +247,8 @@ export const DepositModal = ({ vault, network, onSuccess }: DepositModalProps) =
 					className={cn(
 						buttonVariants(),
 						'bg-blue-500 hover:bg-blue-700 text-white py-2 px-4 rounded-xl transition-colors cursor-pointer'
-					)}>
+					)}
+				>
 					Deposit
 				</span>
 			</DialogTrigger>
@@ -260,7 +261,8 @@ export const DepositModal = ({ vault, network, onSuccess }: DepositModalProps) =
 								onSubmit={form.handleSubmit(async () => {
 									await deposit();
 								})}
-								className="space-y-8">
+								className="space-y-8"
+							>
 								<FormField
 									control={form.control}
 									name="depositAmount"
@@ -303,7 +305,8 @@ export const DepositModal = ({ vault, network, onSuccess }: DepositModalProps) =
 								<Button
 									data-testid="submit-button"
 									type="submit"
-									disabled={!!error || Number(depositAmount) === 0}>
+									disabled={!!error || Number(depositAmount) === 0}
+								>
 									Submit
 								</Button>
 							</form>
@@ -335,7 +338,8 @@ export const DepositModal = ({ vault, network, onSuccess }: DepositModalProps) =
 										<a
 											href={(chain?.blockExplorers.default.url as string) + '/tx/' + depositTxHash}
 											target="_blank"
-											rel="noreferrer">
+											rel="noreferrer"
+										>
 											<Button className="w-fit">View Transaction</Button>
 										</a>
 									)}
@@ -356,7 +360,8 @@ export const DepositModal = ({ vault, network, onSuccess }: DepositModalProps) =
 													  depositState === TokenDepositStatus.WaitingForApprovalConfirmation
 													? 'bg-amber-500 w-12 h-12'
 													: 'bg-emerald-600 w-10 h-10'
-										}`}>
+										}`}
+									>
 										{1}
 									</div>
 									<div className="text-lg">
@@ -388,7 +393,8 @@ export const DepositModal = ({ vault, network, onSuccess }: DepositModalProps) =
 													: depositState === TokenDepositStatus.TokensDeposited
 														? 'bg-emerald-600 w-10 h-10'
 														: 'bg-gray-400 w-10 h-10'
-										}`}>
+										}`}
+									>
 										{2}
 									</div>
 									<div className="text-lg">
@@ -415,7 +421,8 @@ export const DepositModal = ({ vault, network, onSuccess }: DepositModalProps) =
 								<a
 									href={(chain?.blockExplorers.default.url as string) + '/tx/' + depositTxHash}
 									target="_blank"
-									rel="noreferrer">
+									rel="noreferrer"
+								>
 									<Button className="w-fit">View Transaction</Button>
 								</a>
 							)}

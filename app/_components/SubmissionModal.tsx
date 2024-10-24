52301dc9083aba5ae567bbc660aab7c4420d8f48
Jamie Harding <jharding142@icloud.com>
2024-10-24 17:46:45 +0200

    submission modal

--- app/_components/SubmissionModal.tsx
+++ app/_components/SubmissionModal.tsx
@@ -154,8 +154,7 @@ export const SubmissionModal = ({
 													href={ref.url}
 													target="_blank"
 													rel="noreferrer"
-													style={{ color: 'blue', textDecoration: 'underline' }}
-												>
+													style={{ color: 'blue', textDecoration: 'underline' }}>
 													{ref.name}
 												</a>
 											</li>
@@ -172,7 +171,6 @@ export const SubmissionModal = ({
 				}
 			}
 
-			// Check if the user has sufficient token approvals
 			for (const deposit of tokenDeposits) {
 				if (!deposit.tokenInfo) throw new Error(`Token info not found`);
 
@@ -194,7 +192,6 @@ export const SubmissionModal = ({
 						)
 					);
 
-					// Send approval transaction
 					const approveTx = await writeContractAsync({
 						address: deposit.tokenInfo.address,
 						abi: erc20Abi,
@@ -213,7 +210,6 @@ export const SubmissionModal = ({
 						)
 					);
 
-					// Wait for approval transaction confirmation
 					await waitForTransactionReceipt(config.getClient(), {
 						hash: approveTx,
 						confirmations: 1
@@ -268,8 +264,9 @@ export const SubmissionModal = ({
 				});
 				// eslint-disable-next-line @typescript-eslint/no-explicit-any
 			} catch (e: any) {
-				setError(e.details || 'There was an error when confirming the transaction in your wallet.');
-				return;
+				throw new Error(
+					e.details || 'There was an error when confirming the transaction in your wallet.'
+				);
 			}
 
 			setSubmissionState(SubmissionStatus.WaitingForDeploymentConfirmation);
@@ -293,12 +290,9 @@ export const SubmissionModal = ({
 					}`
 				);
 			} else {
-				if (e.message.includes('User rejected the request')) {
-					return setError('User rejected the transaction.');
-				}
-				setError('An error occurred');
+				setError(e.message || 'An unknown error occurred.');
 			}
-			setOpen(false);
+			return setOpen(false);
 		}
 	};
 
@@ -309,8 +303,7 @@ export const SubmissionModal = ({
 					onClick={() => setOpen(true)}
 					color="primary"
 					size="sm"
-					className=" from-blue-600 to-violet-600 bg-gradient-to-br"
-				>
+					className=" from-blue-600 to-violet-600 bg-gradient-to-br">
 					{buttonText}
 				</Button>
 			) : (
@@ -318,8 +311,7 @@ export const SubmissionModal = ({
 			)}
 			<DialogContent
 				onInteractOutside={resetSubmissionState}
-				className="bg-white flex flex-col justify-center w-full font-light gap-y-8"
-			>
+				className="bg-white flex flex-col justify-center w-full font-light gap-y-8">
 				{showDisclaimer && (
 					<div className="flex flex-col items-start gap-y-4">
 						<DialogTitle className="w-full font-light text-2xl">Wait!</DialogTitle>
@@ -359,8 +351,7 @@ export const SubmissionModal = ({
 							onClick={() => {
 								setShowDisclaimer(false);
 								submitStrategy();
-							}}
-						>
+							}}>
 							I understand
 						</Button>
 					</div>
@@ -374,8 +365,7 @@ export const SubmissionModal = ({
 						<div
 							className={`transition-opacity duration-1000 flex flex-col ${
 								submissionState === SubmissionStatus.Done ? 'opacity-0' : 'opacity-100'
-							}`}
-						>
+							}`}>
 							{tokenDeposits.map((deposit, i) => (
 								<div key={i} className="flex items-center my-4">
 									<div
@@ -387,8 +377,7 @@ export const SubmissionModal = ({
 													  deposit.status === TokenDepositStatus.WaitingForApprovalConfirmation
 													? 'bg-amber-500 w-12 h-12'
 													: 'bg-emerald-600 w-10 h-10'
-										}`}
-									>
+										}`}>
 										{i + 1}
 									</div>
 									<div className="text-lg">
@@ -421,8 +410,7 @@ export const SubmissionModal = ({
 												: submissionState === SubmissionStatus.Done
 													? 'bg-emerald-600 w-10 h-10'
 													: 'bg-gray-400 w-10 h-10'
-									}`}
-								>
+									}`}>
 									{tokenDeposits.length + 1}
 								</div>
 								<div className="text-lg">
@@ -449,8 +437,7 @@ export const SubmissionModal = ({
 						</div>
 						<Button
 							className="mt-4"
-							onClick={() => router.push(`${window.location.origin}/my-strategies`)}
-						>
+							onClick={() => router.push(`${window.location.origin}/my-strategies`)}>
 							Track your strategy
 						</Button>
 					</div>

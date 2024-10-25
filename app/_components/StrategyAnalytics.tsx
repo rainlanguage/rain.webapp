c370631ff3fd5ba725eea545943212a798ad1d7d
Jamie Harding <jharding142@icloud.com>
2024-10-25 17:58:44 +0200

    add

--- app/_components/StrategyAnalytics.tsx
+++ app/_components/StrategyAnalytics.tsx
@@ -143,7 +143,8 @@ const StrategyAnalytics = ({ orderHash, network }: props) => {
 									data-testid="strategy-status"
 									size="2xl"
 									className="py-2 px-4"
-									color={query.data.active ? 'green' : 'red'}>
+									color={query.data.active ? 'green' : 'red'}
+								>
 									{query.data.active ? 'Active' : 'Inactive'}
 								</Badge>
 
@@ -153,7 +154,8 @@ const StrategyAnalytics = ({ orderHash, network }: props) => {
 										className={removalStatus !== RemovalStatus.Idle ? 'animate-pulse' : ''}
 										onClick={() => {
 											removeOrder();
-										}}>
+										}}
+									>
 										{removalStatus}
 									</Button>
 								)}

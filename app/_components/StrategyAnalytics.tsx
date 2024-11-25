85f107bbcf9b00a3e6f9ac780095d79eb3673e88
highonhopium <jks.hardy@gmail.com>
2024-11-25 16:05:16 +0000

    wip

--- app/_components/StrategyAnalytics.tsx
+++ app/_components/StrategyAnalytics.tsx
@@ -129,6 +129,9 @@ const StrategyAnalytics = ({ orderHash, network }: props) => {
 		}
 	}, [query]);
 
+	// is the owner of the strategy the connected wallet?
+	const isOwner = address && isAddressEqual(getAddress(address), getAddress(query.data.owner));
+
 	return (
 		<div className="container flex-grow pt-8 pb-safe">
 			{!subgraphUrl && <div data-testid="no-sg-error">No subgraph found for this network</div>}
@@ -144,19 +147,17 @@ const StrategyAnalytics = ({ orderHash, network }: props) => {
 									data-testid="strategy-status"
 									size="2xl"
 									className="py-2 px-4"
-									color={query.data.active ? 'green' : 'red'}
-								>
+									color={query.data.active ? 'green' : 'red'}>
 									{query.data.active ? 'Active' : 'Inactive'}
 								</Badge>
 
-								{query.data.active && (
+								{query.data.active && isOwner && (
 									<Button
 										data-testid="remove-strategy-btn"
 										className={removalStatus !== RemovalStatus.Idle ? 'animate-pulse' : ''}
 										onClick={() => {
 											removeOrder();
-										}}
-									>
+										}}>
 										{removalStatus}
 									</Button>
 								)}
@@ -197,10 +198,7 @@ const StrategyAnalytics = ({ orderHash, network }: props) => {
 													withdraw
 													network={network}
 													onDepositWithdrawSuccess={refetchQuotes}
-													showDepositWithdraw={isAddressEqual(
-														getAddress(address || ''),
-														getAddress(query.data.owner)
-													)}
+													showDepositWithdraw={isOwner}
 												/>
 											</div>
 										);
@@ -219,10 +217,7 @@ const StrategyAnalytics = ({ orderHash, network }: props) => {
 													withdraw
 													network={network}
 													onDepositWithdrawSuccess={refetchQuotes}
-													showDepositWithdraw={isAddressEqual(
-														getAddress(address || ''),
-														getAddress(query.data.owner)
-													)}
+													showDepositWithdraw={isOwner}
 												/>
 											</div>
 										);
